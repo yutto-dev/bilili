42f443944a710ee51ebd6e7d748ac4b0e1303e9a
SigureMo <sigure.qaq@gmail.com>
2020-05-25 01:37:44 +0800

    :white_check_mark: test: add tests and ci

---
+++ tests/default.py
@@ -0,0 +1,12 @@
+config_default =  {
+    "url": "",
+    "dir": "",
+    "qn_seq": [120, 116, 112, 80, 74, 64, 32, 16],
+    "episodes": "all",
+    "playlist_type": "dpl",
+    "playlist_path_type": "RP",
+    "overwrite": False,
+    "block_size": 64*1024*1024,
+    "cookies": {"SESSDATA": None},
+    "segmentation": True
+}
