0624108380a34e54da6dea7e8cda7b06f5cebb03
SigureMo <sigure.qaq@gmail.com>
2020-07-15 23:21:41 +0800

    :recycle: refactor: update repair_filename function

--- common/base.py
+++ common/base.py
@@ -65,8 +65,24 @@ def touch_file(path):
 
 def repair_filename(filename):
     """ 修复不合法的文件名 """
+    def to_full_width_chr(matchobj):
+        char = matchobj.group(0)
+        full_width_char = chr(ord(char) + ord('？') - ord('?'))
+        return full_width_char
+    # 路径非法字符，转全角
     regex_path = re.compile(r'[\\/:*?"<>|]')
-    return regex_path.sub('', filename)
+    # 空格类字符，转空格
+    regex_spaces = re.compile(r'\s+')
+    # 不可打印字符，移除
+    regex_non_printable = re.compile(
+        r'[\001\002\003\004\005\006\007\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f'
+        r'\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a]')
+
+    filename = regex_path.sub(to_full_width_chr, filename)
+    filename = regex_spaces.sub(' ', filename)
+    filename = regex_non_printable.sub('', filename)
+    filename = filename.strip()
+    return filename
 
 
 def get_size(path):
