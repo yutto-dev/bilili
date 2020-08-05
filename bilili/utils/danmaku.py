474843df7f7014dc61e0cd3dd955dd3302a934b7
SigureMo <sigure.qaq@gmail.com>
2020-08-05 19:13:53 +0800

    🐛 fix: can not access raw.githubusercontent.com

--- bilili/utils/danmaku.py
+++ bilili/utils/danmaku.py
@@ -1,11 +1,14 @@
 import os
 import requests
+import zipfile
 
 from bilili.utils.base import touch_dir, touch_file
 
 
 class ASS():
     plugin_url = "https://raw.githubusercontent.com/m13253/danmaku2ass/master/danmaku2ass.py"
+    plugin_zip_url = "https://github.com/m13253/danmaku2ass/archive/master.zip"
+    plugin_zip_path = "plugins/danmaku2ass.zip"
     plugin_path = "plugins/danmaku2ass.py"
     def __init__(self):
         self.has_plugin = os.path.exists(ASS.plugin_path)
@@ -16,9 +19,19 @@ class ASS():
         touch_dir(os.path.dirname(ASS.plugin_path))
         touch_file(os.path.join(os.path.dirname(ASS.plugin_path), "__init__.py"))
         print("下载弹幕转换插件中……")
-        res = requests.get(ASS.plugin_url)
-        with open(ASS.plugin_path, "w", encoding="utf8") as f:
-            f.write(res.text)
+        try:
+            res = requests.get(ASS.plugin_url)
+            plugin_content = res.text
+        except:
+            res = requests.get(ASS.plugin_zip_url)
+            with open(ASS.plugin_zip_path, 'wb') as f:
+                f.write(res.content)
+            plugin_zip = zipfile.ZipFile(ASS.plugin_zip_path, 'r')
+            plugin_content = plugin_zip.read('danmaku2ass-master/danmaku2ass.py').decode('utf-8')
+            os.remove(ASS.plugin_zip_path)
+
+        with open(ASS.plugin_path, "w", encoding="utf-8") as f:
+            f.write(plugin_content)
         self.has_plugin = True
 
     def convert_danmaku_from_xml(self, xml_path, height, width):
